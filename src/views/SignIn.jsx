852d78efada4c69e8f4222ad2190b4a9cda836b8
Pedro Marchiori <pmarchiori08@gmail.com>
2024-11-04 16:58:52 -0300

    tp1_commit

---
+++ src/views/SignIn.jsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+const SignIn = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Sign In
+    </Button>
+  );
+};
+
+export default SignIn;
---
+++ src/views/SignUp.jsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+const SignUp = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Sign Up
+    </Button>
+  );
+};
+
+export default SignUp;
